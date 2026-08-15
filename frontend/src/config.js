cf8eaf28d7782a6e2380a932362029e2108c6509
ApekshaDadhich24 <apekshadadhich24@gmail.com>
2026-08-15 09:04:31 +0530

    backend url added to config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,5 @@
-export const BACKEND_URL = 'http://localhost:5000';
+export const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || 'http://localhost:5000';
 export const AI_URL = 'https://behaviorshield-api-806702562911.us-central1.run.app';
+
+
+
